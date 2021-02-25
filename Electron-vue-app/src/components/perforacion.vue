bbcb5a43b6b1cd2aee938dfe978fa2785e6062d4
Francoviera <vierafranco1@gmail.com>
2021-02-25 00:52:01 -0300

    se acomodo el estilo de las tarjeta de perforacion

--- Electron-vue-app/src/components/perforacion.vue
+++ Electron-vue-app/src/components/perforacion.vue
@@ -13,27 +13,56 @@
         </div>
       </div>   
     <div class="row">
-      <!-- <div class="col-md-12 scroll"> -->
-        <!-- Blog Post -->
-        <div class="col-md-4 mb-4 mt-4">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        </div>
-
-        <div class="col-md-6 mb-4 mt-4">
-            <!-- <div class="card h-150 button-div"> -->
-            <!-- <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a> -->
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">{{perforacion.location}}</a>
-                </h4>
-                <h5>{{perforacion.name}} {{perforacion.surname}}</h5>
-                <p class="card-text">{{perforacion.description}}</p>
+      <div class="col-md-12 mb-4 mt-4">
+        <!-- Card Start -->
+          <div class="card">
+            <div class="row ">
+              <div class="col-md-7 px-3">
+                <small class="text-muted date">{{perforacion.date}}</small>
+                <div class="card-block px-6">
+                  <h3 class="card-title text-primary">{{perforacion.location}}</h3>
+                  <h5 class="card-text">{{perforacion.name}} {{perforacion.surname}}</h5>
+                  <p class="card-text">{{perforacion.description}}</p>
+                  <div class="div-btns-abm">
+                    <button class="btn btn-sm btn-primary ml-2"><i class="fas fa-eye"></i></button>
+                    <button class="btn btn-sm btn-warning ml-2"><i class="fas fa-pencil-alt"></i></button>
+                    <button class="btn btn-sm btn-danger ml-2"><i class="far fa-trash-alt"></i></button>
+                  </div>
+                </div>
               </div>
-              <div class="card-footer">
-                <!-- <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small> -->
-                <small class="text-muted">{{perforacion.date}}</small>
+              <!-- Carousel start -->
+              <div class="col-md-5">
+                <div id="CarouselTest" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#CarouselTest" data-slide-to="0" class="active"></li>
+                    <!-- <li data-target="#CarouselTest" data-slide-to="1"></li>
+                    <li data-target="#CarouselTest" data-slide-to="2"></li> -->
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block imgPerforacion" width="100%" heigth="100%" src="../assets/perforacion.jpg" alt="">
+                    </div>
+                    <!-- <div class="carousel-item">
+                      <img class="d-block" src="https://picsum.photos/450/300?image=855" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block" src="https://picsum.photos/450/300?image=355" alt="">
+                    </div> -->
+                    <a class="carousel-control-prev" href="#CarouselTest" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                      <a class="carousel-control-next" href="#CarouselTest" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                  </div>
+                </div>
               </div>
-            <!-- </div> -->
+              <!-- End of carousel -->
+            </div>
+          </div>
+          <!-- End of card -->
         </div>
       <!-- </div> -->
     </div>
